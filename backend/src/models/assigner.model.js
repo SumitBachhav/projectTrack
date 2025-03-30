7d820d38e5d6277526aea0ceb32cfab45903c79b
PRATIK-RAKTATE <rhutikraktate@gmail.com>
2025-03-30 22:59:36 +0530

    feat: comments

--- backend/src/models/assigner.model.js
+++ backend/src/models/assigner.model.js
@@ -5,54 +5,76 @@ const taskSchema = new Schema(
     title: {
       type: String,
       required: [true, "Title is required"],
-      trim: true
+      trim: true,
     },
     description: {
       type: String,
       required: [true, "Description is required"],
-      trim: true
+      trim: true,
     },
     assigner: {
       type: Schema.Types.ObjectId,
       ref: "User",
-      required: true
+      required: true,
     },
     receiver: {
       type: Schema.Types.ObjectId,
       ref: "User",
-      required: true
+      required: true,
     },
     status: {
       type: String,
       enum: ["pending", "inProgress", "completed", "approved"],
-      default: "pending"
+      default: "pending",
     },
     deadline: {
       type: Date,
-      required: [true, "Deadline is required"]
+      required: [true, "Deadline is required"],
     },
     completedAt: {
-      type: Date
+      type: Date,
     },
-
+    
     remark: {
       type: String,
-      trim: true
+      trim: true,
     },
     previousAssignments: [
       {
         user: {
           type: Schema.Types.ObjectId,
-          ref: "User"
+          ref: "User",
         },
         reassignedAt: {
           type: Date,
-          default: Date.now
-        }
-      }
-    ]
+          default: Date.now,
+        },
+      },
+    ],
+    comments: [
+      {
+        content: {
+          type: String,
+          required: [true, "Comment  required"],
+          trim: true,
+        },
+        commentedBy: {
+          type: Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        createdAt: {
+          type: Date,
+          default: Date.now,
+        },
+        updatedAt: {
+          type: Date,
+        },
+      },
+    ],
   },
-  { timestamps: true }
+
+  { timestamps: true }  
 );
 
 export const Task = mongoose.model("Task", taskSchema);
